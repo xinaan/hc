002b6366cef4e96d687265261413108eee4faa9b
Sinaan <cnatheboy@gmail.com>
2015-09-08 11:46:33 +0500

    init

---
+++ test/controllers/welocme_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class WelocmeControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/helpers/welocme_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class WelocmeHelperTest < ActionView::TestCase
+end
